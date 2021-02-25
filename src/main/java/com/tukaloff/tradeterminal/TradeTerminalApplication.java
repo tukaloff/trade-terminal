6e9d743cdb293a74710707a44993fc2eda9e5555
tukaloff <tukaloff.eugen@gmail.com>
2021-02-25 21:42:25 +0300

    Add trades.json

--- src/main/java/com/tukaloff/tradeterminal/TradeTerminalApplication.java
+++ src/main/java/com/tukaloff/tradeterminal/TradeTerminalApplication.java
@@ -3,8 +3,6 @@ package com.tukaloff.tradeterminal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.swing.*;
-
 @SpringBootApplication
 public class TradeTerminalApplication {
 
